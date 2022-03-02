bf6bcb7c59758f532ade37d2591a79159d022850
Korobskoy Roman <korobskoyra@gmail.com>
2022-03-02 20:45:02 +0300

    fixes

--- KorobskoyRA.playground/Contents.swift
+++ KorobskoyRA.playground/Contents.swift
@@ -1,10 +1,10 @@
 // 1
 // Определить две константы a и b типа Double, присвоить им любые значения. Вычислить среднее значение и сохранить результат в переменную avarage.
 
-let a = 2.5
-let b = 5.0
+let a: Double = 2.5
+let b: Double = 5
 
-var avarage = (a + b) / 2.0
+var avarage = (a + b) / 2
 
 // 2
 // Создать кортеж, и задать два любых строковых значения с названиями firstName и lastName. Далее необходимо вывести в консоль строку в формате "Full name: [firstName] [lastName]".
@@ -15,33 +15,36 @@ print("Full name: \(tuple.firstName) \(tuple.lastName)")
 // 3
 // Создать две опциональные переменные типа Float. Одной из них задать первоначальное значение. Написать функцию, которая принимает на вход опциональную переменную типа Float. Функция должна безопасно извлечь значение из входящей переменной. Если значение удалось получить - необходимо вывести его в консоль, если значение у переменной отсутствует вывести в консоль фразу "Variable can't be unwrapped". Вызвать функцию дважды с двумя ранее созданными переменными.
 
-var floatA: Float? = nil
+var floatA: Float?
 var floatB: Float? = 2.5
 
 func checkNil(number: Float?) {
-    print(number ?? "Variable can't be unwrapped")
+    if let number = number {
+        print(number)
+    } else {
+        print("Variable can't be unwrapped")
+    }
 }
+
 checkNil(number: floatA)
 checkNil(number: floatB)
 
 // 4
 // Напишите программу для вывода первых 15 чисел последовательности Фибоначчи
 
-func fibonacci() {
+func fibonacci(n: Int) {
     var num1 = 0
     var num2 = 1
-    print(num1)
-    print(num2)
     
-    for _ in 0 ..< 13 {
+    for _ in 0 ..< n {
+        print(num1)
         let num = num1 + num2
         num1 = num2
         num2 = num
-        print(num2)
     }
 }
 
-fibonacci()
+fibonacci(n: 15)
 
 // 5
 // Напишите программу для сортировки массива, использующую метод пузырька. Сортировка должна происходить в отдельной функции, принимающей на вход исходный массив.
@@ -58,6 +61,7 @@ func sort(array: [Int]) -> [Int] {
     print(sortedArray)
     return sortedArray
 }
+
 sort(array: [3,1,2,6,5,7,8,7,6,5])
 
 // 6
@@ -65,21 +69,23 @@ sort(array: [3,1,2,6,5,7,8,7,6,5])
 
 func convertString(string: String) -> String {
     var mutateString = string
-    var lastChar = Int(String(mutateString[mutateString.index(before: mutateString.endIndex)])) // определяем последний символ
-    print(lastChar!)
-    if lastChar != nil && lastChar != 9 { // проверяем наличие
-        lastChar! += 1 // прибавляем 1 к последнему символу
+    let lastChar = Int(String(mutateString[mutateString.index(before: mutateString.endIndex)])) // определяем последний символ
+    guard var lastChar = lastChar else { return "" }
+    print(lastChar)
+    if lastChar != 9 { // проверяем наличие
+        lastChar += 1 // прибавляем 1 к последнему символу
         mutateString.removeLast() // удаляем старый
-        mutateString.insert(Character("\(lastChar!)"), at: mutateString.endIndex) // вставляем новый на место старого
+        mutateString.insert(Character("\(lastChar)"), at: mutateString.endIndex) // вставляем новый на место старого
     } else if lastChar == 9 { // проверяем последнюю цифру
         lastChar = 0 // обнуляем до десятка
-        var preLastChar = Int(String(mutateString[mutateString.index(mutateString.startIndex, offsetBy: mutateString.count - 2)])) // вычисляем предпоследний
-        preLastChar! += 1 // увеличиваем на 1
-        print(preLastChar!)
+        let preLastChar = Int(String(mutateString[mutateString.index(mutateString.startIndex, offsetBy: mutateString.count - 2)])) // вычисляем предпоследний
+        guard var preLastChar = preLastChar else { return "" }
+        preLastChar += 1 // увеличиваем на 1
+        print(preLastChar)
         mutateString.remove(at: mutateString.index(mutateString.startIndex, offsetBy: mutateString.count - 2)) // удаляем старый предпоследний символ
-        mutateString.insert(Character("\(preLastChar!)"), at: mutateString.index(mutateString.startIndex, offsetBy: mutateString.count - 1)) // добавляем предпоследний
+        mutateString.insert(Character("\(preLastChar)"), at: mutateString.index(mutateString.startIndex, offsetBy: mutateString.count - 1)) // добавляем предпоследний
         mutateString.removeLast() // удаляем старый последний
-        mutateString.insert(Character("\(lastChar!)"), at: mutateString.endIndex) // вставляем новый на место старого
+        mutateString.insert(Character("\(lastChar)"), at: mutateString.endIndex) // вставляем новый на место старого
     }
     return String(mutateString)
 }
