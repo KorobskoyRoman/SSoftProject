fe4401c774b4ade909adbfd6de4169110d2667be
KorobskoyRoman <shopone023@gmail.com>
2022-07-20 01:11:15 +0300

    Constants + stackView extension

--- SSoftProject/Resourses/Constants.swift
+++ SSoftProject/Resourses/Constants.swift
@@ -5,12 +5,22 @@
 //  Created by Roman Korobskoy on 19.07.2022.
 //
 
+import UIKit
+
 enum JSONConstants {
     static let categoriesJson = "Categories.json"
+    static let eventsJson = "Events.json"
+}
 
-    static let childsJson = "ChildsEvents.json"
-    static let adultsJson = "AdultsEvents.json"
-    static let agedJson = "AgedEvents.json"
-    static let animalsJson = "AnimalsEvents.json"
-    static let eventsJson = "EventsOfEvents.json"
+enum ConstraintsConst {
+    static let inset20: CGFloat = 20
+    static let inset10: CGFloat = 10
+    static let inset15: CGFloat = 15
+    static let inset40: CGFloat = 40
+    static let inset8: CGFloat = 8
+    static let inset16: CGFloat = 16
+    static let inset22: CGFloat = 22
+    static let inset33: CGFloat = 33
+    static let inset17: CGFloat = 17
+    static let inset32: CGFloat = 32
 }
