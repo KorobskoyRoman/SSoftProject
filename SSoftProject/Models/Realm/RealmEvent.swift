2d581cd276045d3c0e63b18a42837c4546db158c
KorobskoyRoman <shopone023@gmail.com>
2022-07-28 17:42:41 +0300

    Added realm models

---
+++ SSoftProject/Models/Realm/RealmEvent.swift
@@ -0,0 +1,22 @@
+//
+//  RealmEvent.swift
+//  SSoftProject
+//
+//  Created by Roman Korobskoy on 27.07.2022.
+//
+
+import RealmSwift
+
+class RealmEvent: Object, Decodable {
+    @Persisted var id: Int
+    @Persisted var image: String
+    @Persisted var title: String
+    @Persisted var details: String
+    @Persisted var date: String
+    @Persisted var isDone: Bool
+    @Persisted var category: String
+    @Persisted var address: String
+    @Persisted var phone: String
+    @Persisted var details2: String
+    @Persisted var details3: String
+}
