4cc36248557f40dca01e9c5212a4cd9d1b38a3d6
KorobskoyRoman <shopone023@gmail.com>
2022-07-31 18:19:40 +0300

    Deleted root struct

--- SSoftProject/Models/Categories.swift
+++ SSoftProject/Models/Categories.swift
@@ -5,10 +5,6 @@
 //  Created by Roman Korobskoy on 12.07.2022.
 //
 
-struct Root: Codable {
-    let results: [Categories]
-}
-
 struct Categories: Codable, Hashable {
     let id: Int64
     let image: String
