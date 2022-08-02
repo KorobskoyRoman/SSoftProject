4b9b551d27fb78319c45e958d62aa13170add731
KorobskoyRoman <shopone023@gmail.com>
2022-08-02 20:50:08 +0300

    Added categories core data class

---
+++ CategoriesCD+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CategoriesCD+CoreDataClass.swift
+//  SSoftProject
+//
+//  Created by Roman Korobskoy on 01.08.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class CategoriesCD: NSManagedObject {
+
+}
---
+++ CategoriesCD+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  CategoriesCD+CoreDataProperties.swift
+//  SSoftProject
+//
+//  Created by Roman Korobskoy on 01.08.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension CategoriesCD {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CategoriesCD> {
+        return NSFetchRequest<CategoriesCD>(entityName: "CategoriesCD")
+    }
+
+    @NSManaged public var id: Int64
+    @NSManaged public var image: String?
+    @NSManaged public var title: String?
+
+}
+
+extension CategoriesCD : Identifiable {
+
+}
